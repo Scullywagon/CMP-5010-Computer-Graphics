ba7ee8cd386bcb314eb58ba0f4d47f80660f4663
Logan Scully <accounts@loganscully.com>
2025-04-28 16:46:58 +0100

    fuck

---
+++ cw2/build/CMakeFiles/cw.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "/Users/logan/University/CMP-5010-Computer-Graphics/cw2/output/cw"
+  "/Users/logan/University/CMP-5010-Computer-Graphics/cw2/output/cw.pdb"
+  "CMakeFiles/cw.dir/src/BoundingTree.cpp.o"
+  "CMakeFiles/cw.dir/src/BoundingTree.cpp.o.d"
+  "CMakeFiles/cw.dir/src/CollisionManager.cpp.o"
+  "CMakeFiles/cw.dir/src/CollisionManager.cpp.o.d"
+  "CMakeFiles/cw.dir/src/Entities/FerrisWheel.cpp.o"
+  "CMakeFiles/cw.dir/src/Entities/FerrisWheel.cpp.o.d"
+  "CMakeFiles/cw.dir/src/Entity.cpp.o"
+  "CMakeFiles/cw.dir/src/Entity.cpp.o.d"
+  "CMakeFiles/cw.dir/src/Renderer.cpp.o"
+  "CMakeFiles/cw.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/cw.dir/src/Scene.cpp.o"
+  "CMakeFiles/cw.dir/src/Scene.cpp.o.d"
+  "CMakeFiles/cw.dir/src/TexFormFile.cpp.o"
+  "CMakeFiles/cw.dir/src/TexFormFile.cpp.o.d"
+  "CMakeFiles/cw.dir/src/main.cpp.o"
+  "CMakeFiles/cw.dir/src/main.cpp.o.d"
+  "CMakeFiles/cw.dir/src/stb_impl.cpp.o"
+  "CMakeFiles/cw.dir/src/stb_impl.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
